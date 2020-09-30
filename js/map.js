ad34d05f28450fc6ff68ac48ff6b21dbdca88666
f-eckhart <florian.eckhart@gmail.com>
2020-09-30 17:19:55 +0200

    added states to map

--- js/map.js
+++ js/map.js
@@ -1,5 +1,24 @@
-let map = L.map('map').setView([47.2692124, 11.4041024], 8);
+initializeMap();
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+function initializeMap() {
+    let map = L.map('map').setView([47.2692124, 11.4041024], 9);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    drawStates(map);
+}
+
+function drawStates(map) {
+    fetch('data/laender_999_geo.json', {mode:'no-cors'})
+        .then(response => response.json())
+        .then(data =>
+            L.geoJSON(data, {
+                style: {
+                    "color": "#ff0000",
+                    "opacity": 0.5
+                }
+            }).addTo(map)
+        );
+}
