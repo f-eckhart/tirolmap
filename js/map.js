5f81947f5aeafcff945936aaf8bfe56be969b85e
f-eckhart <florian.eckhart@gmail.com>
2020-09-30 16:13:03 +0200

    changed center of the map to Innsbruck

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([47.2692124, 11.4041024], 8);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
